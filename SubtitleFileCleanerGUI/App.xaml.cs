bb0b86a6a22013f8286574b636b68d84185aa381
spiganvlad <spiganvlad@gmail.com>
2023-03-23 11:27:40 +0200

    Add DI hosting

--- SubtitleFileCleanerGUI/App.xaml.cs
+++ SubtitleFileCleanerGUI/App.xaml.cs
@@ -1,17 +1,48 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SubtitleFileCleanerGUI.View;
 
 namespace SubtitleFileCleanerGUI
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = new HostBuilder()
+                .ConfigureServices(ConfigureServices)
+                .ConfigureAppConfiguration(ConfigureAppConfiguration)
+                .Build();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+        }
+
+        private void ConfigureAppConfiguration(IConfigurationBuilder builder)
+        {
+            //Implement configuration
+        }
+
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            using var serviceScope = _host.Services.CreateScope();
+            var services = serviceScope.ServiceProvider;
+
+            try
+            {
+                var mainWindow = services.GetRequiredService<MainWindow>();
+                mainWindow.Show();
+            }
+            catch (Exception ex)
+            {
+                //Implement logging
+            }
+        }
     }
 }
